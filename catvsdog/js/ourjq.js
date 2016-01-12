27973d987f5b4616197a288b74cae14f3352c834
Chris Kuiper and Joseph Arnstein <chris.kuiper@gmail.com>
2016-01-12 11:22:24 -0800

    change or function from show img to toggle and change id tags to classes and add .first to clickables to prevent multiple events.

--- catvsdog/js/ourjq.js
+++ catvsdog/js/ourjq.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
   $("button#speak").click(function() {
-    $("ul#user").prepend("<li>Meow!</li>");
-    $("ul#webpage").prepend("<li>Bark!</li>");
-    $('li').click(function() {
-      $("img#cat").show();
-    });
+    $("ul#user").prepend("<li class=kitten>Meow!</li>");
+    $("ul#webpage").prepend("<li class=puppy>Bark!</li>");
+      $('li.kitten').first().click(function() {
+        $("img#cat").toggle();
+      });
+      $('li.puppy').first().click(function() {
+        $("img#dog").toggle();
+      });
   });
-
 });
