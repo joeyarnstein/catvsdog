13925fc0ad41d722d525b4729ab2ec37e032ae92
Chris Kuiper and Joseph Arnstein <joeyarnstein@gmail.com>
2016-01-12 10:37:16 -0800

    identifier to cat image, connect jquery, add clickable function to element li to reveal cat img not displayed.

--- catvsdog/js/ourjq.js
+++ catvsdog/js/ourjq.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
   $("button#speak").click(function() {
     $("ul#user").prepend("<li>Meow!</li>");
     $("ul#webpage").prepend("<li>Bark!</li>");
+    $('li').click(function() {
+      $("img#cat").show();
+    });
   });
+
 });
